d91e4b202e39f1dfce740ec76ba0fae3dc8f5bfe
MayukhBanerjee <mayukh647@gmail.com>
2026-02-24 03:19:08 +0530

    practiced a chatbot with my style

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
 from dotenv import load_dotenv
-from langchain_core.prompts import ChatTemplate
+from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 
+import streamlit as st
 
 #Step 1 : Load ENV
 load_dotenv()
 
 #Step 2 : Create a Chat Template
-prompt = ChatTemplate(
+prompt = ChatPromptTemplate(
     [
         ("system", "You are a cool and useful assistant at the end of every querry you will be answering it in a puzzle manner."),
         ("user", "{question}")
@@ -18,6 +19,17 @@ prompt = ChatTemplate(
 
 llm = ChatGoogleGenerativeAI(
     temparature = 1.0,
-    model = "gemini-flash-2.5"
+    model = "gemini-2.5-flash"
 )
 
+
+st.title("Hehe")
+input = st.text_input("What do you want to know?")
+
+
+output = StrOutputParser()
+
+chain = prompt|llm|output
+
+if input:
+    st.write(chain.invoke({"question":input}))
