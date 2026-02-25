99203abbfcd53bc6be337a4e1c75d9e0e31beadb
MayukhBanerjee <mayukh647@gmail.com>
2026-02-25 19:29:11 +0530

    fixed Essay on poem version

--- api/client.py
+++ api/client.py
@@ -1,13 +1,18 @@
 import requests
 import streamlit as st
 
-def get_gemini_response(input):
+def get_gemini_response_for_essays(input):
     #call the essay url
-    response= requests.post("http://localhost:8000/essay/invoke",
+    response= requests.post("http://localhost:8000/essay/invoke", #API URL which is responsible in calling the essay version
     json = {"input":{"topic":input}})
 
     return response.json()["output"]["content"]
 
+
+def get_gemini_response_for_poems(input1):
+    response1 = requests.post("http://localhost:8000/poem/invoke",
+    json = {"input":{"topic":input1}})
+    return response1.json()["output"]["content"]
 
 
 st.title("Langchain Model")
@@ -15,7 +20,7 @@ input = st.text_input("Write an essay on: ")
 input1 = st.text_input("Write a poem on: ")
 
 if input:
-    st.write(get_gemini_response(input))
+    st.write(get_gemini_response_for_essays(input))
 
 if input1:
-    st.write(get_gemini_response(input1))
+    st.write(get_gemini_response_for_poems(input1))
