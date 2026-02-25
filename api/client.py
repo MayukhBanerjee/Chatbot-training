7cfbd5675e5e34b735bd31bcabfcde2d31e5a754
MayukhBanerjee <mayukh647@gmail.com>
2026-02-25 19:04:21 +0530

    Made a bot that write poems and essays using FastAPI

--- api/client.py
+++ api/client.py
@@ -2,7 +2,20 @@ import requests
 import streamlit as st
 
 def get_gemini_response(input):
-    response= requests.post("")
+    #call the essay url
+    response= requests.post("http://localhost:8000/essay/invoke",
+    json = {"input":{"topic":input}})
+
+    return response.json()["output"]["content"]
 
 
-def 
+
+st.title("Langchain Model")
+input = st.text_input("Write an essay on: ")
+input1 = st.text_input("Write a poem on: ")
+
+if input:
+    st.write(get_gemini_response(input))
+
+if input1:
+    st.write(get_gemini_response(input1))
